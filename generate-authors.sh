2665ce0b4208bbcefacd56089c603b71b8c3e1f0
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-12-08 13:41:40 -0800

    updated lastest generate_authors.sh version

--- generate-authors.sh
+++ generate-authors.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-echo "# This file lists all individuals having contributed" \
-"content to the repository." > AUTHORS
-echo "# For how it is generated, see \`hack/generate-authors.sh\`." >> AUTHORS
-git log --format=fuller | grep "Author:" | sort -u | cut -c 13- >> AUTHORS
+echo "Joaquin Victoria <3942@holberton.com>" > AUTHORS
+echo "Salvador Diaz <3940@holbertonschool.com>" >> AUTHORS
